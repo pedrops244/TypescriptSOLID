0b69c468f1864bb7a37f979b6218dceb9d1369c5
Pedro Souza <pedrops244@hotmail.com>
2023-11-12 10:07:18 -0300

    applying openclosed principle to create a discount

---
+++ src/ocp/classes/order.ts
@@ -0,0 +1,29 @@
+import { Messages } from '../services/messages';
+import { Persistency } from '../services/persistency';
+import { ShoppingCart } from './shoppingCart';
+
+export class Order {
+  private _orderStatus: OrderSatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly msg: Messages,
+    private readonly persistency: Persistency,
+  ) {}
+
+  get orderStatus(): Readonly<OrderSatus> {
+    return this._orderStatus;
+  }
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio!');
+      return;
+    }
+    this._orderStatus = 'closed';
+    this.msg.sendMessage(
+      `Seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido!`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
